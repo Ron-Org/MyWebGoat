f1d0b750f9b04d1f8441b762a68c6880717944ea
ronibary <117287407+ronibary@users.noreply.github.com>
2022-12-29 12:14:16 +0200

    Update CSRFIntegrationTest.java

--- src/it/java/org/owasp/webgoat/CSRFIntegrationTest.java
+++ src/it/java/org/owasp/webgoat/CSRFIntegrationTest.java
@@ -16,7 +16,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.HashMap;
+import java.util.HashMap ;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
